60d50e731c6e061b3c291d2f09acb749bcc940f0
fireduck64 <fireduck@gmail.com>
2020-09-15 23:32:54 -0700

    Use settings from storage

--- chrome-snow-socks-extension/background.js
+++ chrome-snow-socks-extension/background.js
@@ -2,34 +2,65 @@
 'use strict';
 
 
-console.log("Loading");
+if (!localStorage.firstime)
+{
+  localStorage.host='localhost';
+  localStorage.port=1080;
+  localStorage.enabled=1;
 
-var config = {
-  mode: "fixed_servers",
-  rules: {
-    proxyForHttp: {
-      scheme: "socks5",
-      host: "localhost",
-      port: 1080
-    },
+  localStorage.firsttime = 1;
+}
+
+localStorage.enabled=1;
+
+loadProxy();
+
+function loadProxy() {
+
+  console.log("Loading");
+  console.log(localStorage);
+
+  var config_socks = {
+    mode: "fixed_servers",
+    rules: {
+      proxyForHttp: {
+        scheme: "socks5",
+        host: localStorage.host,
+        port: parseInt(localStorage.port)
+      },
+    }
+  };
+
+  var config_disable = {
+    mode: "system",
+  };
+
+
+  var icon = {
+    path: "icons/flower-on-128.png"
   }
-};
- 
-console.log(JSON.stringify(config));
-chrome.proxy.settings.set(
-  {value: config, scope: 'regular'},
-  function() {});
 
-chrome.proxy.settings.get(
-  {'incognito': false},
-  function(config2) {console.log(JSON.stringify(config2));});
+  if (parseInt(localStorage.enabled)==1)
+  {
+    console.log(JSON.stringify(config_socks));
+    chrome.proxy.settings.set(
+      {value: config_socks, scope: 'regular'},
+      function() {});
+  }
+  else
+  {
+    console.log("Disabling proxy");
+    chrome.proxy.settings.set(
+      {value: config_disable, scope: 'regular'},
+      function() {});
+    icon["path"] = "icons/flower-off-128.png";
+  }
 
-var icon_on = {
-  path: 'icons/flower-on.png'
+  chrome.proxy.settings.get(
+    {'incognito': false},
+    function(config2) {console.log(JSON.stringify(config2));});
+  console.log(icon);
+
+  chrome.browserAction.setIcon(icon);
 }
-var icon_off = {
-  path: 'icons/flower-off.png'
-}
-
-//chrome.browserAction.setIcon(icon_off);
 
